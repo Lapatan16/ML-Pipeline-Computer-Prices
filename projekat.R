7e1beac03b264c41646fc3a4094857f4f5827697
Mateja Lapatanovic <matejalapatanovic03@gmail.com>
2025-11-12 17:27:30 +0100

    Uradio sam opis za sve

--- projekat.R
+++ projekat.R
@@ -16,6 +16,13 @@ str(data) ## prikazuje koliko redova i kolona ima u skupu i tip podatka svake pr
 summary(data$price) ## sve osnovne podatke o price srednja vr medijana min max i slicno
 
 # Histogram ciljne promenljive price
+
+# Histogram prikazuje raspodelu ciljne promenljive price. Ovde takođe možemo videti 
+# da raspodela nije simetrična, već pozitivno asimetrična. Kao što je malopre 
+# spomenuto najveći broj uređaja se nalazi u srednjem cenovnom rangu, manji u nižem, 
+# a najmanji broj uređaja u skupljem cenovnom rangu. Ovo je i očekivano jer većina 
+# korisnika kupuje upravo uređaje srednjeg ranga. 
+
 ggplot(data, aes(x = price)) +
   geom_histogram(bins = 50, fill = "#1f78b4", color = "black", alpha = 0.7) +
   scale_x_continuous(labels = comma) +
@@ -33,6 +40,18 @@ ggplot(data, aes(x = price)) +
 ## ovo theme samo centrira i bolduje naslov nista vise
 
 # Boxplot za outliere
+
+# Boxplot i potvrđuje ono što smo videli na histogramu i ovde vidimo tačke koje na 
+# histogramu nisu bile prikazane, jer ih je jako malo sa visokom cenom i te tačke 
+# potencijalno predstavljaju outlier-e. Oni mogu značajno da utiču na to koliko je naš 
+# model dobar što će i biti provereno kasnije.
+# Pošto linearna regresija pretpostavlja da je raspodela ciljne promenljive približno 
+# normalna, a kod nas to nije slučaj i  zbog toga će kasnije biti primenjena logaritamska
+# transformacija. Ona približava raspodelu normalnoj tj. čini da histogram ima zvonastu 
+# strukturu. Samim tim se smanjuje uticaj skupljih uređaja i model postaje bolji i 
+# stabilniji.
+
+
 ggplot(data, aes(y = price)) +
   geom_boxplot(fill = "steelblue", alpha = 0.7) +
   scale_y_continuous(labels = comma) +
@@ -47,6 +66,15 @@ ggplot(data, aes(y = price)) +
 # Vizuelizacija podataka (crtamo grafike kako bismo izvukli korisne informacije, nakon toga ih obradjujemo)
 
 # boxplot cene uređaja u odnosu na godinu izdavanja, godine se krecu od 2018 do 2025
+
+# Na osnovu boxplot-ova možemo videti da su medijane uglavnom slične kroz godine ili 
+# se minimalno povećavaju. Iako bi se trebalo pretpostaviti da će noviji uređaji biti 
+# malo skuplji ovo je sasvim u redu. Postoji dosta tačaka koje odskaču od većine, to 
+# su sve verovatno profesionalni uređaji sa veoma jakim performansama i skupim 
+# komponentama, ali će kasnije biti ispitani. Iako modeli srednjeg cenovnog ranga 
+# blago rastu jeftini modeli uglavnom ostaju sličnih cena što znači da u tom delu ne 
+# dolazi do nekih većih promena.
+
 ggplot(data, aes(x = factor(release_year), y = price)) +
   geom_boxplot(fill = "#1f78b4", color = "black", alpha = 0.7, outlier.colour = "red") +
   labs(
@@ -61,6 +89,12 @@ ggplot(data, aes(x = factor(release_year), y = price)) +
   )
 
 # boxplot cene uređaja u odnosu na tip
+
+# Na osnovu boxplot dijagrama možemo videti da postoji razlika u ceni između 
+# desktop računara i laptopova, ali nije velika. Medijana cene laptopova je nešto 
+# veća što je i očekivano, jer laptopovi imaju integrisane komponente i prenosivi su, 
+# pa su skuplji od računara. Postoji dosta cena koje odskaču od većine (srednje klase) 
+# i to su verovatno profesionalni računari i gaming laptopovi.
 
 ggplot(data, aes(x = device_type, y = price, fill = device_type)) +
   geom_boxplot(alpha = 0.8, outlier.colour = "red", outlier.shape = 8) +
@@ -77,6 +111,14 @@ ggplot(data, aes(x = device_type, y = price, fill = device_type)) +
   )
 
 # grafik cene u odnosu na ram memoriju
+
+# Sa grafika možemo videti da tačke formiraju vertikalne linije što znači da imamo 
+# samo određene vrednosti koje mogu biti vrednosti RAM memorije i posle ćemo moći 
+# ovu numeričku promenljivu da prebacimo u kategorijsku. Postoje uređaji koji imaju 
+# nešto manje od 100GB RAM-a, a dosta su skuplji nego uređaji sa preko 100GB RAM-a, 
+# može biti da su ostale komponente dosta skuplje npr. procesor, grafička kartica, 
+# eksterna memorija, itd…
+
 ggplot(data, aes(x = ram_gb, y = price)) +
   geom_point(color = "black", size = 1) +
   labs(
@@ -90,6 +132,15 @@ ggplot(data, aes(x = ram_gb, y = price)) +
   )
 
 # grafik cene u odnosu na broj jezgara procesora
+
+# Sa grafika se može videti da sa porastom broja jezgara blago raste i cena uređaja što 
+# je i logično jer broj jezgara je indikator procesorske moći, ali tako je negde do 12 
+# ili 16 jezgara i nakon toga se cena uglavnom smanjuje sa povećanjem broja jezgara što 
+# je potrebno ispitati. Možda neke druge komponente imaju uticaj kod uređaja sa manje 
+# od 20 jezgara. Takođe postoji nekoliko primera sa manje od 10 jezgara, a cenom preko 
+# 8000$ i verovatno je isti razlog kao za ove prethodne, a takođe je moguće da je došlo 
+# do nekih grešaka pri unosu, što je potrebno dodatno ispitati.
+
 ggplot(data, aes(x = cpu_cores, y = price)) +
   geom_point(alpha = 0.4) +
   labs(
@@ -104,6 +155,16 @@ ggplot(data, aes(x = cpu_cores, y = price)) +
   )
 
 # boxplot cene u zavisnosti od ranga procesora
+
+# Sa grafika možemo videti boxplot-ove koji prikazuju cene uređaja u odnosu na klasu 
+# procesora koju imaju. 1 je najlošija, 6 je najbolja klasa. Medijana se porastom klase 
+# postepeno povećava, što je i logično, što je bolji procesor cena je veća. Za svaku 
+# kategoriju postoje potencijalni outlieri tj. cene dosta niže ili više od prosečnih za 
+# tu kategoriju, što je u redu , jer su druge komponente najverovatnije jeftinije. 
+# Postoje uređaji najviše klase, dosta jeftiniji od uređaja nižih klasa, to može značiti 
+# da je neka druga komponenta zaslužna za takvu cenu, a može biti i da su podaci 
+# pogrešno uneti.
+
 ggplot(data, aes(x = as.factor(cpu_tier), y = price)) +
   geom_boxplot(alpha = 0.7) +
   labs(
@@ -114,7 +175,13 @@ ggplot(data, aes(x = as.factor(cpu_tier), y = price)) +
   theme_minimal(base_size = 14) +
   theme(plot.title = element_text(hjust = 0.5, face = "bold"))
 
-# boxplot cne u zavisnosti od ranga grafičke kartice
+# boxplot cene u zavisnosti od ranga grafičke kartice
+
+# Sa grafika možemo videti kakve su cene uređaja u odnosu na grafičku karticu i 
+# takođe kao i kod procesora postoji 6 kategorija i još pravilnije sa porastom klase 
+# raste i cena. Potencijalnih outliera ima, ali biće ispitani, posebno ovaj uređaj sa 
+# cenom od 7500$, a najlošijom klasom grafičke kartice. U globalu ova osobina je dosta 
+# bitna za krajnju cenu uređaja.
 
 ggplot(data, aes(x = as.factor(gpu_tier), y = price)) +
   geom_boxplot() +
@@ -128,6 +195,13 @@ ggplot(data, aes(x = as.factor(gpu_tier), y = price)) +
 
 # Grafik odnosa cene u odnosu na vram memoriju
 
+# VRAM memorija jeste u stvari zasebna memorija grafičke kartice, koja se koristi 
+# za generisanje slika, koje se prikazuju na ekranu. Više ovakvog ram-a znači i 
+# veću cenu, ali nije sasvim pravilno, postoje uređaji sa malo VRAM-a sa izuzetno 
+# visokim cenama. Što se tiče potencijalnih outlier-a postoje uređaji sa 0GB VRAM-a, 
+# moguće da nemaju uopšte VRAM i da budu poprilično skupi.  Svaki deo ima outlier-e 
+# ali su svi u granicama normale.
+
 ggplot(data, aes(x = vram_gb, y = price)) +
   geom_point() +
   labs(
@@ -140,6 +214,11 @@ ggplot(data, aes(x = vram_gb, y = price)) +
 
 # Grafik cene u zavisnosti od količine memorije
 
+# Standardne vrednosti količine memorije jesu upravo 512GB i 1TB što grafik, 
+# sa slike i potvrđuje, većina uređaja se nalazi u ovom opsegu. Svakako je velika 
+# razlika u ceni u  zavisnosti od tipa memorije (SSD ili HDD). Nekih nerealnih 
+# vrednosti uglavnom nema.
+
 ggplot(data, aes(x = storage_gb, y = price)) +
   geom_point() +
   labs(
@@ -151,6 +230,15 @@ ggplot(data, aes(x = storage_gb, y = price)) +
   theme(plot.title = element_text(hjust = 0.5, face = "bold"))
 
 # Boxplot-ovi cene u zavisnosti od tipa memorije
+
+# Boxplot-ovi prikazuju zavisnost cene od tipa memorije, HDD je najstarija i najsporija, 
+# SSD novija od nje i brža, Hybrid je njihova kombinacija, a NVMe je najbrža i najbolja 
+# verzija SSD memorije. Ovo obeležje ne utiče značajno na cenu uređaja, medijane su 
+# skoro pa slične, malo je medijana niža kod HDD memorije što je i logično jer je to 
+# najsporija i najjeftinija memorija. Postoje uređaji sa velikom cenom, a najslabijom 
+# vrstom memorije, ali je verovatno ima dosta, jer su ostale komponente skuplje i jače. 
+# Dobar primer je RAM, ako imamo mnogo RAM-a, onda nam brzina eksterne memorije nije 
+# presudna, te je bolje uzeti sporiju i jeftiniju eksternu memoriju.
 
 ggplot(data, aes(x = storage_type, y = price)) +
   geom_boxplot() +
@@ -165,6 +253,13 @@ ggplot(data, aes(x = storage_type, y = price)) +
 
 # Grafik cene uređaja u odnosu na veličinu ekrana
 
+# Grafik prikazuje zavisnost cene u odnosu na veličinu ekrana izraženu u inčima. 
+# U skupu podataka je više laptopova, jer je većina uređaja između 15 i 17 inča, što 
+# je karakteristično za laptopove. Veličina ekrana ima uticaj na cenu, ali ne preteran, 
+# uticaj je verovatno veći u kombinaciji sa drugim obeležjima. Postoje laptopovi sa 
+# cenama preko 8000$ što je malo čudno, ali veličina ekrana i nije bitan indikator u 
+# svetu laptopova i tehnologije, već kakva je unutrašnjost uređaja.
+
 ggplot(data, aes(x = display_size_in, y = price)) +
   geom_point() +
   labs(
@@ -175,7 +270,13 @@ ggplot(data, aes(x = display_size_in, y = price)) +
   theme_minimal(base_size = 14) +
   theme(plot.title = element_text(hjust = 0.5, face = "bold"))
 
-# Grafik cene uređaja u odnosu na rezolicuju
+# Grafik cene uređaja u odnosu na rezoluciju
+
+# Sa grafika vidimo da medijana blago raste sve do rezolucije 2880x1800, nakon toga 
+# opada i na samom kraju ponovo raste. Kod svake rezolucije postoje jako skupi modeli 
+# što je sasvim u redu. Takođe je bitno napomenuti da ima nekoliko uređaja sa izuzetno 
+# malom cenom oko 500$, a sa najvećom rezolucijom 3840X2160, što ne prati trend nikako 
+# i može predstaviti veliki problem u treniranju modela.
 
 ggplot(data, aes(x = resolution, y = price)) +
   geom_boxplot(fill = "lightblue", color = "black") +
@@ -192,6 +293,10 @@ ggplot(data, aes(x = resolution, y = price)) +
 
 # grafik zavisnosti cene od frekvencije osvezavanja tj refresh rate-a
 
+# Sa grafika se vidi da najveći broj uređaja ima 60Hz frekvenciju osvežavanja ekrana 
+# i cena je uglavnom u srednjem i nižem rangu. Sa povećanjem frekvencije ne raste cena,
+# za svaku frekvenciju većina uređaja ima cenu do 4000$, dosta većih vrednosti ima svugde, najviše kod 60 i 120Hz. Nema nekog pravilnog povećanja ili smanjenja, pa ovo obeležje i nije toliko bitno.
+
 ggplot(data, aes(x = refresh_hz, y = price)) +
   geom_point(alpha = 0.5, color = "black") +
   labs(
@@ -205,6 +310,12 @@ ggplot(data, aes(x = refresh_hz, y = price)) +
   )
 
 # grafik zavisnosti cene od kapaciteta baterije uređaja
+
+# Sa grafika vidimo uređaje sa 0Wh baterije i to su desktop računari svi ostali uređaji
+# su laptopovi. Sa većim kapacitetom baterije bi cene trebale da rastu , ali to baš i 
+# nije linearno malo rastu, malo se smanjuju. Postoje laptopovi sa oko 60Wh baterijom 
+# cene preko 7000$ i potrebno je ovakve uređaje ispitati. Takođe treba proveriti 
+# uređaje ispod 700$ sa skoro 100Wh.
 
 ggplot(data, aes(x = battery_wh, y = price)) +
   geom_point() +
@@ -222,6 +333,11 @@ ggplot(data, aes(x = battery_wh, y = price)) +
 
 # Grafik zavisnosti cene uređaja od njegove težine
 
+# Sa grafika vidimo da je većina uređaja težine od 1 do 3.5kg i ima ih sa raznim cenama. 
+# Uređaji manji od kilogram mogu potencijalno biti outlieri i potrebno ih je dodatno 
+# ispitati. Težina uglavnom i nije neko merilo cene, postoji dosta laganih uređaja 
+# raznih cena, isto važi i za dosta teže uređaje. 
+
 ggplot(data, aes(x = weight_kg, y = price)) +
   geom_point() +
   labs(
@@ -237,6 +353,11 @@ ggplot(data, aes(x = weight_kg, y = price)) +
 
 # Grafik zavisnosti cene od brzine procesora
 
+# Sa grafika vidimo da veza cene i brzine procesora i nije bas linearna, postepeno 
+# raste do određenog dela, ali ima i dosta uređaja sa velikom brzinom procesora ,a 
+# malom cenom, u redu je ako su im npr. druge komponente jeftinije ili može takođe 
+# zavisiti od proizvođača procesora.
+
 ggplot(data, aes(x = cpu_base_ghz, y = price)) +
   geom_point() +
   labs(
@@ -250,7 +371,12 @@ ggplot(data, aes(x = cpu_base_ghz, y = price)) +
     panel.grid.minor = element_blank()
   )
 
-# Grafik zavisnosti cene od brzine procesora
+# Cena uređaja u odnosu na brend procesora
+
+# Možemo videti cene uređaja u odnosu na proizvođača procesora. Apple prednjači u 
+# odnosu na AMD i Intel što je i očekivano, jer oni obično i dolaze sa skupljim 
+# komponentama. Svi imaju dosta skupe uređaje što je potrebno ispitati, posebno 
+# uređaje iznad 9 hiljada dolara, kojih je samo nekoliko.
 
 ggplot(data, aes(x = cpu_brand, y = price)) +
   geom_boxplot() +
@@ -267,6 +393,14 @@ ggplot(data, aes(x = cpu_brand, y = price)) +
 
 # Boxplot-ovi zavisnosti cene u odnosu na operativni sistem uređaja
 
+# Medijana ChromeOS je najniža što je i očekivano jer je relativno mlad operativni 
+# sistem, sa ne toliko širokom upotrebom, a uređaji sa macOS su očekivano najskuplji 
+# zbog cene svojih komponenti i svog uticaja na tržištu. Uređaja sa dosta većim 
+# cenama ima svugde, najviše sa Windows operativnim sistemom, jer je on najpoznatiji 
+# i najkorišćeniji operativni sistem. Izdvaja se uređaj koji ima macOS i cenu ispod 
+# 1000$ i uređaj sa ChromeOS i cenom preko 6000$. To su ekstremne vrednosti, koje bi 
+# mogle značajno da umanje preciznost modela.
+
 ggplot(data, aes(x = os, y = price)) +
   geom_boxplot() +
   labs(
@@ -281,6 +415,12 @@ ggplot(data, aes(x = os, y = price)) +
   )
 
 # Boxplot-ovi zavisnosti cene u odnosu na proizvođača uređaja
+
+# Medijane su uglavnom slične, Razer ima malo veću od ostalih, a Apple dosta ali 
+# i očekivano, Apple uređaji su uvek najskuplji. Kod HP, Lenovo i MSI brenda postoje 
+# modeli skuplji od 9000$ što je potrebno ispitati i potencijalni outlieri jesu i 
+# modeli Dell i Razer marke koji su jeftiniji od 700$ dolara. Ovo je važan prediktor, 
+# ali je uglavnom dosta bitniji u kombinaciji sa drugim prediktorima.
 
 ggplot(data, aes(x = brand, y = price)) +
   geom_boxplot() +
